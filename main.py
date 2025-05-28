a4f1971bab5ad82048be9638506d1f49eecf6e8c
Iryna Pobortseva <ipobortseva@kse.org.ua>
2025-05-28 10:28:06 +0300

    add new

---
+++ main.py
@@ -0,0 +1,23 @@
+#task 1, it prints my variable
+H = ("Hello, Python World!")
+print (H)
+#task 2, does mathematical 
+a = int (input())
+b = int (input())
+print (a + b, a - b, a * b, a / b)
+#task 3
+c = ("горіх")
+d = ("котик")
+words = c + " " + d
+print (words)
+print (len (words))
+#task 4
+n = float(input())
+if n%2 == 0:
+    print ("Парне")
+else:
+    print ("Непарне")
+#task 5
+for i in range (1, 11):
+    print(i)
+#task 6
